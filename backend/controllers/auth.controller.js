1513f7dd2eab704d2aec76536e7dc7ce419c8da9
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-06-10 22:14:07 +0530

    Added Auth Controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const User = require ('../models/user')
 const register = async (req, res) => {
     try {
         const { email, password, username } = req.body
-
+        
         const user = await User.create({
             email,
             password,
@@ -15,3 +15,29 @@ const register = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    else
+    res.status(200).json({
+      message: "Login successful",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
