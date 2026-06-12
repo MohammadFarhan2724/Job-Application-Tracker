00a96efc3339b8f962b07546e3d6b85c0ec1c97e
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-06-12 15:09:02 +0530

    Added Authorization

--- backend/routes/application.routes.js
+++ backend/routes/application.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require ('../middleware/auth.middleware')
 
 const {
     createApplication,
@@ -9,10 +10,10 @@ const {
     deleteApplication
 } = require ('../controllers/application.controller')
 
-router.post('/', createApplication)
-router.get('/', getApplication)
-router.get('/:id', getApplicationById)
-router.patch('/:id', updateApplication)
-router.delete('/:id', deleteApplication)
+router.post('/', authMiddleware, createApplication)
+router.get('/', authMiddleware, getApplication)
+router.get('/:id', authMiddleware, getApplicationById)
+router.patch('/:id', authMiddleware, updateApplication)
+router.delete('/:id', authMiddleware, deleteApplication)
 
 module.exports = router
