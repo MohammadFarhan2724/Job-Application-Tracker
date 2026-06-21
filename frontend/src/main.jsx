4689dc9f61fdf3e0c1b667a58ad7ad9b2d6ff91a
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-06-21 14:22:43 +0530

    Updated frontend files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from '@tanstack/react-query';
 import './index.css'
 import App from './App.jsx'
+import Register from './pages/Register.jsx';
+
+const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  { path: "/register", 
+    element: <Register /> 
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+  <RouterProvider router={router} />
+  </QueryClientProvider>
+  </StrictMode>
 )
