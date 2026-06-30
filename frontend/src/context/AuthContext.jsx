b3ecfa38abf8f5aeac6802dce57e2ad3c77c4b23
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-06-30 16:49:10 +0530

    Added AuthContext

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext({token: null, user: null, login: () => {}, logout: () => {}})
+
+export const AuthProvider = ({children}) => {
+  const [token, setToken] = useState(localStorage.getItem('token') || null)
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')) || null)
+
+  const login = (newToken, newUser) => {
+    setToken(newToken)
+    setUser(newUser)
+    localStorage.setItem('token', newToken)
+    localStorage.setItem('user', JSON.stringify(newUser))
+  }
+
+  const logout = () => {
+    setToken(null)
+    setUser(null)
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+  }
+
+  return (
+    <AuthContext.Provider value={{ token, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
+
+export default AuthContext
