f75983106295f67775ef9244d367873cb13e7bb0
MohammadFarhan2724 <anjumfarhan2724@gmail.com>
2026-07-09 01:10:19 +0530

    Updated routes for gmail automation

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,13 @@
-const express = require ('express')
+const express = require('express')
 const router = express.Router()
 const {validateRegister, validateLogin} = require('../middleware/validate.middleware')
-const {register, login} = require ('../controllers/auth.controller')
+const authMiddleware = require('../middleware/auth.middleware')
+const {register, login, googleAuthRedirect, googleAuthCallback} = require('../controllers/auth.controller')
 
-router.post('/register',validateRegister, register)
-router.post('/login',validateLogin, login)
+router.post('/register', validateRegister, register)
+router.post('/login', validateLogin, login)
 
+router.get('/google', authMiddleware, googleAuthRedirect)
+router.get('/google/callback', googleAuthCallback)
 
 module.exports = router;
